13596e01ea327da2991ba83f58be9feeee8f9ea2
F1ame12 <13779081529@163.com>
2023-04-13 16:02:46 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "common/std_basic.h"
-
 #include "structs/mylist.h"
 
 int main()
@@ -9,16 +8,4 @@ int main()
 
     return 0;
 
-}
-
-//生成随机二进制文件的函数
-void generateRandomBinaryFile(const char* filename, int size)
-{
-    std::ofstream out(filename, std::ios::binary);
-    for (int i = 0; i < size; i++)
-    {
-        char c = rand() % 256;
-        out.write(&c, 1);
-    }
-    out.close();
 }
