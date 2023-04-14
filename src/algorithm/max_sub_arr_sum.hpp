92808a854ade55a010a6a98f633e6e206ef9de2a
F1ame12 <13779081529@163.com>
2023-04-14 14:42:38 +0800

    leetcode 53

--- src/algorithm/max_sub_arr_sum.hpp
+++ src/algorithm/max_sub_arr_sum.hpp
@@ -4,55 +4,39 @@
 
 using std::vector;
 
+/**
+ * LeetCode 53. 最大子序和
+*/
+
+
 class Solution
 {
 public:
-    struct SumKey
-    {
-        int start;
-        int end;
-
-        SumKey(int start, int end) : start(start), end(end) {}
-        SumKey() : start(0), end(0) {}
-
-        bool operator<(const SumKey& other) const
-        {
-            if (this->start < other.start) return true;
-            if (this->end < other.end) return true;
-            return false; 
-        }
-    };
-
-    std::map<SumKey, int> sumMap;
-
-    int maxSum = std::numeric_limits<int>::min();
-    void calcSubSum(const vector<int> &nums, int start, int end)
-    {
-        if (start > end) return;
-
-        int sum = 0;
-        if (sumMap.find(SumKey(start, end)) != sumMap.end())
-        {
-            sum = sumMap[SumKey(start, end)];
-        }
-        else
-        {
-            for (int i = start; i <= end; i++)
-            {
-                sum += nums[i];
-            }
-            sumMap[SumKey(start, end)] = sum;
-        }
-        if (sum > maxSum) maxSum = sum;
-
-        calcSubSum(nums, start, end - 1);
-        calcSubSum(nums, start + 1, end);
-    }
-
     int maxSubArray(vector<int> &nums)
     {
-        calcSubSum(nums, 0, nums.size() - 1);
 
-        return maxSum;
+        return 0;
+    }
+};
+
+
+/**
+ * 动态规划方法
+*/
+class Solution1
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int maxNum = std::numeric_limits<int>::min();
+
+        int perSum = 0;
+        for (const int &num : nums)
+        {
+            perSum = std::max(perSum + num, num);
+            maxNum = std::max(maxNum, perSum);
+        }
+
+        return maxNum;
     }
 };
