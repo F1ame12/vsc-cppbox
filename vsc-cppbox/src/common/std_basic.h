9d94f972cd69aca965685485dc6b8135f1fd754d
F1ame12 <13779081529@163.com>
2023-04-17 18:27:51 +0800

    try upload commit by hand

---
+++ vsc-cppbox/src/common/std_basic.h
@@ -0,0 +1,19 @@
+#ifndef __STD_BASIC_H__
+#define __STD_BASIC_H__
+
+#include <cstddef>
+#include <bitset>
+#include <random>
+#include <ctime>
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <limits>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+
+#endif
