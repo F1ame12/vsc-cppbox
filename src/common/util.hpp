c014667e9b3b0bfb0e2c08e402ce9ce359a55356
F1ame12 <13779081529@163.com>
2023-12-26 12:04:54 +0800

    add util func

--- src/common/util.hpp
+++ src/common/util.hpp
@@ -7,15 +7,16 @@ namespace Util
 {
     const std::string randStr(const int maxLen)
     {
-        static const std::string basicStr {"abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTVWXYZ0123456789"};
+        static const std::string basicStr{"abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTVWXYZ0123456789"};
         static std::random_device rd;
         static std::default_random_engine engine(rd());
 
-        if (maxLen <= 0) return "";
-        
+        if (maxLen <= 0)
+            return "";
+
         std::uniform_int_distribution ud(0, static_cast<int>(basicStr.length() - 1));
 
-        char buffer[maxLen] {};
+        char buffer[maxLen]{};
         for (int i = 0; i < maxLen; i++)
         {
             const int randPos = ud(engine);
@@ -24,7 +25,18 @@ namespace Util
 
         return std::string(buffer);
     }
-}
 
+    /**
+     * 判断主机字节序是否是小端
+    */
+    bool IsLittleEndian()
+    {
+        const int n = 1;
+        const char *p = (char *)(&n);
+        if (*p == 1)
+            return true;
+        return false;
+    }
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,41 +3,14 @@
 #include "./test/testing_box.hpp"
 #include "./test/struct_test.hpp"
 
-void CreateMap(int MapData[100][100])
-{
-    std::random_device rd;
-    std::mt19937_64 mt(rd());
-    std::uniform_int_distribution uni(1, 100);
-    for (int i = 0; i < 100; i++)
-    {
-        for (int j = 0; j < 100; j++)
-        {
-            int randN = uni(mt);
-            if (randN <= 30)
-            {
-                MapData[i][j] = 0;
-            }
-            else
-            {
-                MapData[i][j] = 1;
-            }
-        }
-    }
-}
+
+
+
 
 int main()
 {
-    int Data[100][100];
-    CreateMap(Data);
 
-    for (int i = 0; i < 100; i++)
-    {
-        for (int j = 0; j < 100; j++)
-        {
-            std::cout << Data[i][j];
-        }
-        std::cout << std::endl;
-    }
+    const bool isLittle = IsLittleEndian();
 
     return 0;
 }
